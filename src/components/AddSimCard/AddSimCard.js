4338521a41587841e335bf7e22025b9d8854ce6f
Hasan75 <hasanjab14@gmail.com>
2022-04-07 04:58:14 +0600

    table added

--- src/components/AddSimCard/AddSimCard.js
+++ src/components/AddSimCard/AddSimCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MaterialTable from 'material-table';
 import addSimStyles from './AddSimCard.module.css';
 
 const AddSimCard = () => {
@@ -55,7 +56,6 @@ const AddSimCard = () => {
             />
           </div>
         </div>
-
         <div class='form-row row my-1'>
           <div class='form-group d-flex flex-row justify-content-center align-items-center col-md-3'>
             <label className='me-1' for='vendor'>
@@ -82,6 +82,45 @@ const AddSimCard = () => {
             <input type='text' class='form-control' id='phonePlan' />
           </div>
         </div>
+      </div>
+      <div className={`${addSimStyles.singleInput}`}>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Advance Search'
+        />
+      </div>
+      <div className='mui-table my-3'>
+        <MaterialTable
+          title='Basic Selection Preview'
+          columns={[
+            { title: 'Name', field: 'name' },
+            { title: 'Surname', field: 'surname' },
+            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+            {
+              title: 'Birth Place',
+              field: 'birthCity',
+              lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+            },
+          ]}
+          data={[
+            {
+              name: 'Mehmet',
+              surname: 'Baran',
+              birthYear: 1987,
+              birthCity: 63,
+            },
+            {
+              name: 'Zerya Betül',
+              surname: 'Baran',
+              birthYear: 2017,
+              birthCity: 34,
+            },
+          ]}
+          options={{
+            selection: true,
+          }}
+        />
       </div>
     </section>
   );
