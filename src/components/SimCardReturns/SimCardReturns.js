f7ac9d9be63aa45aa81c3d9776d33f7e770d2f2a
Hasan75 <hasanjab14@gmail.com>
2022-04-11 05:51:24 +0600

    sim returns added

--- src/components/SimCardReturns/SimCardReturns.js
+++ src/components/SimCardReturns/SimCardReturns.js
@@ -2,10 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { Button } from '@mui/material';
 import Box from '@mui/material/Box';
 import MaterialTable from 'material-table';
+import { Link } from 'react-router-dom';
 
 const SimCardReturns = () => {
   const [columns, setColumns] = useState([
-    { title: 'ID', field: 'id', hidden: true },
+    {
+      title: 'ID',
+      field: 'id',
+      hidden: false,
+      render: (row) => (
+        <Link
+          sx={{ textDecoration: 'none' }}
+          to={`/dashboard/simCardDetails/${row.id}`}
+        >
+          {row.id}
+        </Link>
+      ),
+    },
     {
       title: 'Return Date',
       field: 'return_date',
@@ -13,35 +26,47 @@ const SimCardReturns = () => {
     },
     { title: 'Return Reason', field: 'return_reason' },
     { title: 'Agent', field: 'agent' },
+    { title: 'Distributor', field: 'distributor' },
+    { title: 'item', field: 'item', hidden: true },
+    { title: 'SSID', field: 'ssid', hidden: true },
+    { title: 'Device Serial Number', field: 'device_serial_num', hidden: true },
+    { title: 'IMEI-1', field: 'imei1', type: 'numeric', hidden: true },
+    { title: 'IMEI-2', field: 'imei2', type: 'numeric', hidden: true },
     {
-      title: 'Status',
-      field: 'status',
-      lookup: { 1: 'Active', 2: 'Deactive' },
+      title: 'Shipping Methods',
+      field: 'ship_method',
+      lookup: { 1: 'USPS', 2: 'UPS', 3: 'FedEx', 4: 'DHL', 5: 'In Persion' },
+      hidden: true,
     },
-
     {
-      title: 'Created Date',
-      field: 'created_date',
+      title: 'Tracking Number',
+      field: 'trackingNumber',
+      type: 'numeric',
+      hidden: true,
+    },
+    { title: 'Report Status', field: 'report_status', hidden: true },
+    {
+      title: 'Receiption Date',
+      field: 'receiption_date',
       type: 'date',
       hidden: true,
     },
-    { title: 'Description', field: 'description', hidden: true },
-  ]);
-  const [data, setData] = useState([
-    {
-      name: 'PWR',
-      service_carrier: 'NameX',
-      plan_code: 12345,
-      status: 1,
-    },
-    {
-      name: '321Com',
-      service_carrier: 'NameY',
-      plan_code: 67892,
-      status: 2,
-    },
+    { title: 'Notes', field: 'notes', hidden: true },
   ]);
   // const [errorMessages, setErrorMessages] = useState([]);
+  const [data, setData] = useState([
+    {
+      id: '1011',
+      return_date: '20.10.22',
+      return_reason: 'There was no internet connection',
+    },
+    {
+      id: '2202',
+      return_date: '20.10.22',
+      return_date: '20.10.22',
+      return_reason: 'There was no internet connection',
+    },
+  ]);
 
   return (
     <Box
@@ -125,29 +150,10 @@ const SimCardReturns = () => {
                     padding: '2px 8px',
                   }}
                 >
-                  Activate
+                  View
                 </button>
               ),
               tooltip: 'Click to active',
-              onClick: (data) => console.log(data),
-              // isFreeAction: true,
-            },
-            {
-              icon: () => (
-                <button
-                  style={{
-                    fontSize: '1rem',
-                    borderRadius: '2px',
-                    backgroundColor: '#dddddd',
-                    color: 'black',
-                    border: 'none',
-                    padding: '2px 8px',
-                  }}
-                >
-                  Deactivate
-                </button>
-              ),
-              tooltip: 'Click to Deactive',
               onClick: (data) => console.log(data),
               // isFreeAction: true,
             },
