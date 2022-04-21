ba2f117c4d3c661b8d578ee083de7f7c0ad00ef5
hasan75 <hasanjab14@gmail.com>
2022-04-21 16:46:04 +0600

    imported data from najim

--- src/components/SimCardDetails/SimCardDetails.js
+++ src/components/SimCardDetails/SimCardDetails.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Simcard from './Simcard/Simcard';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Phonecarrier from './Phonecarrier/Phonecarrier';
+import ACP from './ACP Operations/ACP Operations';
+import Note from './Note/Note';
+import ServiceCarrier from './SerivceCarrier/ServiceCarrier';
+import Simop from './Simop/Simop';
+// TAB
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+// END
 
 const SimCardDetails = () => {
   const id = useParams();
